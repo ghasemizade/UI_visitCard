601d05fe122a51b9749810261def12a067c29822
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-18 14:06:58 +0330

    update js

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-let btn = document.getElementsByClassName("btn");
-        function changeColor(color) {
-            btn = --clr;
-        }
+const blue = document.getElementById('blue')
+const red = document.getElementById('red')
+const green = document.getElementById('green')
+const purple = document.getElementById('purple')
+const yellow = document.getElementById('yellow')
+const getHeader = document.getElementById('header')
+
+function changeColor(which) {
+    switch (which.target.id) {
+        case 'blue':
+            getHeader.style.backgroundColor = 'var(--theme-color-blue)'
+            break;
+        case 'red':
+            getHeader.style.backgroundColor = 'var(--theme-color-red)'
+            break;
+        case 'green':
+            getHeader.style.backgroundColor = 'var(--theme-color-green)'
+            break;
+        case 'purple':
+            getHeader.style.backgroundColor = 'var(--theme-color-purple)'
+            break;
+        case 'yellow':
+            getHeader.style.backgroundColor = 'var(--theme-color-yellow)'
+            break;                            
+        default:
+            break;
+    }
+}
+
+
+blue.addEventListener('click', changeColor)
+red.addEventListener('click', changeColor)
+green.addEventListener('click', changeColor)
+purple.addEventListener('click', changeColor)
+yellow.addEventListener('click', changeColor)
