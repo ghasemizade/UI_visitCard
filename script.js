d72adc8e610a6894969347704954c07f66aa9e2d
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-18 14:14:35 +0330

    update

--- script.js
+++ script.js
@@ -4,27 +4,35 @@ const green = document.getElementById('green')
 const purple = document.getElementById('purple')
 const yellow = document.getElementById('yellow')
 const getHeader = document.getElementById('header')
+const getVisitCard = document.getElementById('visitCard')
 
 function changeColor(which) {
     switch (which.target.id) {
         case 'blue':
             getHeader.style.backgroundColor = 'var(--theme-color-blue)'
+            getVisitCard.style.border = '2px solid var(--theme-color-blue)'
             break;
         case 'red':
             getHeader.style.backgroundColor = 'var(--theme-color-red)'
+            getVisitCard.style.border = '2px solid var(--theme-color-red)'
             break;
         case 'green':
             getHeader.style.backgroundColor = 'var(--theme-color-green)'
+            getVisitCard.style.border = '2px solid var(--theme-color-green)'
             break;
         case 'purple':
             getHeader.style.backgroundColor = 'var(--theme-color-purple)'
+            getVisitCard.style.border = '2px solid var(--theme-color-purple)'
             break;
         case 'yellow':
             getHeader.style.backgroundColor = 'var(--theme-color-yellow)'
+            getVisitCard.style.border = '2px solid var(--theme-color-yellow)'
             break;                            
         default:
             break;
     }
+    console.log(getVisitCard.style.border)
+    
 }
 
 
